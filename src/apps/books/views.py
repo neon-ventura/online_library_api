ff82518d4218217b98fff0644fc9ce0ca88e3149
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2025-05-08 20:03:49 -0300

    method_post_created

--- src/apps/books/views.py
+++ src/apps/books/views.py
@@ -1,38 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from datetime import datetime, timezone
 
 
 books_list = [
   {
     "id": 1,
-    "name": "A Jornada do Dev",
+    "name": "As 48 leis do poder",
     "author": "Lucas Andrade",
+    "link": "https://masculinistaopressoroficial.wordpress.com/wp-content/uploads/2017/06/as-48-leis-do-poder-robert-greene.pdf",
     "created_at": "2025-05-01T10:23:45Z"
-  },
-  {
-    "id": 2,
-    "name": "Python para Todos",
-    "author": "Marina Souza",
-    "created_at": "2025-05-02T14:15:30Z"
-  },
-  {
-    "id": 3,
-    "name": "O Código Limpo",
-    "author": "Carlos Silva",
-    "created_at": "2025-05-03T08:40:12Z"
-  },
-  {
-    "id": 4,
-    "name": "Django na Prática",
-    "author": "Fernanda Ribeiro",
-    "created_at": "2025-05-04T11:00:00Z"
-  },
-  {
-    "id": 5,
-    "name": "RESTful APIs do Zero",
-    "author": "João Mendes",
-    "created_at": "2025-05-05T09:12:59Z"
   }
 ]
 
@@ -41,3 +19,27 @@ class Books (APIView):
     def get(self, req):
         print(f"Get is OK: {status.HTTP_200_OK}")
         return Response({"books": books_list}, status.HTTP_200_OK)
+    
+    def post(self, req):
+        id = req.data.get('id')
+        name = req.data.get('name')
+        author = req.data.get('author')
+        link = req.data.get('link')
+
+        if not name or not author:
+            return Response({"Error: ": "Name and author is required"}, status.HTTP_400_BAD_REQUEST)
+
+        current_date = datetime.now(timezone.utc).isoformat()
+
+        new_book = {
+            "id": id,
+            "name" : name,
+            "author": author,
+            "link": link,
+            "created_at": current_date
+        }
+
+        books_list.append(new_book)
+
+        return Response({"book_created": new_book}, status.HTTP_201_CREATED)
+
