378a08c374ddef3937468644885a28d4fe9332d2
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2025-05-09 13:37:34 -0300

    Add_DB_Postgree

--- src/apps/books/views.py
+++ src/apps/books/views.py
@@ -2,26 +2,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from datetime import datetime, timezone
-
-
-books_list = [
-  {
-    "id": 1,
-    "name": "As 48 leis do poder",
-    "author": "Lucas Andrade",
-    "link": "https://masculinistaopressoroficial.wordpress.com/wp-content/uploads/2017/06/as-48-leis-do-poder-robert-greene.pdf",
-    "created_at": "2025-05-01T10:23:45Z"
-  }
-]
+from .models import Book
+from .serializers import BookSerializer
 
 
 class Books (APIView):
     def get(self, req):
-        print(f"Get is OK: {status.HTTP_200_OK}")
-        return Response({"books": books_list}, status.HTTP_200_OK)
+        
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response({"books": serializer.data}, status.HTTP_200_OK)
     
     def post(self, req):
-        id = req.data.get('id')
         name = req.data.get('name')
         author = req.data.get('author')
         link = req.data.get('link')
@@ -29,17 +21,14 @@ class Books (APIView):
         if not name or not author:
             return Response({"Error: ": "Name and author is required"}, status.HTTP_400_BAD_REQUEST)
 
-        current_date = datetime.now(timezone.utc).isoformat()
+        new_book = Book.objects.create(
+          name=name,
+          author=author,
+          link=link,
+          created_at=datetime.now(timezone.utc)
+        )
+        
+        serializer = BookSerializer(new_book)
 
-        new_book = {
-            "id": id,
-            "name" : name,
-            "author": author,
-            "link": link,
-            "created_at": current_date
-        }
-
-        books_list.append(new_book)
-
-        return Response({"book_created": new_book}, status.HTTP_201_CREATED)
+        return Response({"book_created": serializer}, status.HTTP_201_CREATED)
 
--- src/config/settings.py
+++ src/config/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY_DB = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': SECRET_KEY_DB,
+        'HOST': 'centerbeam.proxy.rlwy.net',
+        'PORT': 32650
     }
 }
 
