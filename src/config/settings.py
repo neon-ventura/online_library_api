7a40d9dd27c011a66ebfa9bc0486054ce598401a
Neon <neonventura@gmail.com>
2025-07-02 23:07:18 +0700

    Users apps created

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.books'
+    'apps.books',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.books.views import BooksView 
+from apps.books.views import BooksView
+from apps.users.views import UsersView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/books/', BooksView.as_view()),
-    path('api/books/<int:pk>', BooksView.as_view())
+    path('api/books/<int:pk>', BooksView.as_view()),
+    path('api/users/', UsersView.as_view())
 ]
